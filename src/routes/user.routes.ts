d88f00fc564f0c153a7d6fc45801b4cc4c29e71e
justinmihigo <mihigojustin28@gmail.com>
2024-11-05 22:23:54 +0200

    feat: create a update controller for the users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, getToken, getUser, getUsers } from "../controllers/user.controller";
+import { createUser, getToken, getUser, getUsers, updateUser } from "../controllers/user.controller";
 import VerifyToken from "../middleware/auth.middleware";
 const router = Router();
 
@@ -10,5 +10,7 @@ router.get("/getUsers",getUsers);
 router.get("/getUser/:id", VerifyToken, getUser);
 
 router.get("/token",getToken);
+
+router.patch("/updateUser/:id", VerifyToken, updateUser);
 
 export default router;
